3e021aa18a3c1c3d491fa06d81de057d3458156d
Zaeem14 <ahmadzaeemzahid@gmail.com>
2024-11-22 16:52:53 +0000

    working to put some css

--- scripts/script.js
+++ scripts/script.js
@@ -13,7 +13,7 @@ function addTask(todo) {
     const taskElement = document.createElement('div');
     taskElement.classList.add('task-container');
     taskElement.innerHTML = `
-        <span>${todo}</span>
+        <span class="taskName">${todo}</span>
         <button class="delete-button js-delete-button">Delete</button>
  
     `;
